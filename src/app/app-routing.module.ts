3bef647076d1dec0f9555b72b7007e09a5e15829
kaik5252 <kaikdossantossilva2@gmail.com>
2023-05-17 16:55:58 -0300

    Aulas-01

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './page/home/home.component';
+import { MenuComponent } from './page/menu/menu.component';
+import { SobreComponent } from './page/sobre/sobre.component';
+import { ContatoComponent } from './page/contato/contato.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'menu', component: MenuComponent },
+  { path: 'sobre', component: SobreComponent },
+  { path: 'contato', component: ContatoComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
