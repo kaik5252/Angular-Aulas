407f479012dd16f0e842beb376656f61a42b199e
Kaik D' Andrade <kaikdossantossilva2@gmail.com>
2023-05-29 16:50:49 -0300

    Aula-06

---
+++ api-rast/src/app/service/servicos.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+Observable
+HttpClient
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ServicosService {
+
+  private url: string = "http://localhost:3000/alunos/";
+
+  constructor(private http: HttpClient) { }
+
+  listarAlunos(): Observable<any> {
+    return this.http.get(`${this.url}`);
+  }
+}
