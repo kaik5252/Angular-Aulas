6efacf81eb656bfbbead06121a34882aa4eeb21c
kaik5252 <kaikdossantossilva2@gmail.com>
2023-06-19 16:42:54 -0300

    Aula-13

--- avaliacao-final/src/app/pag/signup/signup.component.ts
+++ avaliacao-final/src/app/pag/signup/signup.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { BgService } from 'src/app/service/bg.service';
+import { CepService } from 'src/app/service/cep.service';
 
 @Component({
   selector: 'app-signup',
@@ -6,5 +8,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent {
+
+  cep: any;
+  logradouro: any;
+  localidade: any;
+  bairro: any;
+  uf: any;
   
+  constructor(private bgService: BgService, private cepService: CepService) { }
+
+  buscarCep() {
+    this.cepService.getCep(this.cep).subscribe((e) => {
+      this.logradouro = e.logradouro;
+      this.localidade = e.localidade;
+      this.bairro = e.bairro;
+      this.uf = e.uf;
+    });
+  }
 }
