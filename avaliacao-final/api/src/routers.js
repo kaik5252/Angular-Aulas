eeb77d424b0646820c65f4b9a1e7daa2c3c0fbfc
kaik5252 <kaikdossantossilva2@gmail.com>
2023-06-23 13:25:35 -0300

    Aula-10

--- avaliacao-final/api/src/routers.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const controller = require('./database/dbController');
-
-router.post("/", controller.adicionarDados);
-
-module.exports = router;
--- avaliacao-final/src/app/service/user.model.ts
+++
@@ -1,9 +0,0 @@
-export class UserModel {
-    nome!: any;
-    nasc!: any;
-    cep!: any;
-    logradouro!: any;
-    localidade!: any;
-    bairro!: any;
-    uf!: any;
-}
